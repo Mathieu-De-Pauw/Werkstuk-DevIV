0b173d6a3699bc26e14de12c978ca7a883018e86
Mathieu De Pauw <mathieu.de.pauw@student.ehb.be>
2020-05-23 21:26:53 +0200

    FilterDoelgroep

--- jsoncall.js
+++ jsoncall.js
@@ -1,44 +1,61 @@
-console.log("start");
 $(document).ready(function(){
-    console.log("script linked");
+    //console.log("script linked");
+    const optreden =[];
 
     $.ajax({
         type: 'GET',
         url:'/entries.json',
         dataType: 'json'
     }).done(function(data){
-        console.log(data);
+        //console.log(data);
         printData(data);
     }).fail(function(err1,err2){
         console.log(err1,err2);
     }).always(function(){
-        console.log("en");
+        //console.log("en");
     })
 
-      
+    // Verzamel alle data uit json file en steek deze in een array  
     function printData(data){
-        console.log("printing data");
+        $("#insert").empty();
         var g = 0;
         for(var i in data){
-            var stuk = data[i];
-            console.log(stuk);
+            var stuk = data[i];  
+            //console.log(stuk);
             for(var g in stuk){
                 var naam = stuk[g];
+                optreden.push(naam);
                 $("#insert").append('<div class="filterDiv '+naam.category+'"> <br>' + naam.name + "<br>" + naam.excerpt +"<br>" + naam.category + "<br>" + naam['genre-v2']+ "<br>"+ naam["video-length"] +" <br> </div>");
             }
         }
     }
 
-    function filterSelection(data){
-        if(data == 'familie'){
-            console.log('familie geselecteerd');
-        } else if(data == 'volwassenen') {
-            console.log('volwassen geselecteerd');
-        } else {
-            console.log('niks gebeurd');
+    // Kies filter
+    $(".btn-doelgroep").click(function(){
+        const text = $(this).text();
+        console.log(text,optreden);
+        zoekDoelgroep(text,optreden);
+    })    
+
+    // Selecteer data voor gekozen filter
+    function zoekDoelgroep(data,optreden){
+        //console.log(optreden.length);
+        $("#insert").empty();
+        for(let i = 0 ; i < optreden.length ; i++){
+            //console.log(optreden[i]);
+            if(data == optreden[i].category){
+            console.log("ja");
+            maakLayout(optreden[i]);
+            } else {
+            console.log("neen");
+            }
         }
     }
 
+    // Print geselecteerde filter
+    function maakLayout(data){
+        $("#insert").append('<div class="filterDiv '+data.category+'"> <br>' + data.name + "<br>" + data.excerpt +"<br>" + data.category + "<br>" + data['genre-v2']+ "<br>"+ data["video-length"] +" <br> </div>");
+    }
 });
 
 
